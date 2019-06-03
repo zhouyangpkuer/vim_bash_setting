e99b3791d0c8c520859cce02fdbf2420ede72c0e
yangzhou <yangzhou@node-1.two-netbricks.lambda-mpi-pg0.clemson.cloudlab.us>
2019-06-02 20:41:33 -0400

    add setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+sudo apt-get update
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 sudo apt-get -y install vim-nox vim-gtk vim-gnome vim-athena
 
+cp .vimrc ~/
+sudo apt-get -y install byobu
+
 echo "export TERM=screen-256color-bce" >> ~/.bashrc
-echo "do: source ~/.bashrc"
+echo "please do: source ~/.bashrc"
+
+
